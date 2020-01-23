ba3eff1a4906cf294c54b982d6dec945675475fc
sttic <tommyd3689@gmail.com>
2020-01-23 17:42:29 -0500

    add styling to highlight current day

--- src/views/ScheduleView.tsx
+++ src/views/ScheduleView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ScheduleView.css';
 
+import { IEventDay } from '../interfaces';
 import { firstDay, secondDay, thirdDay } from '../data/schedule';
 
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -9,16 +10,26 @@ import Button from 'react-bootstrap/Button';
 import TimelineComponent from '../components/TimelineComponent';
 import EventList from '../components/EventList';
 
+const days: [IEventDay, IEventDay, IEventDay] = [firstDay, secondDay, thirdDay];
+
 const ScheduleView: React.FC = () => {
 	const [day, setDay] = React.useState(firstDay);
+	const [button, setButton] = React.useState(0);
+
+	const updateDay = (day: IEventDay, button: number) => {
+		setDay(day);
+		setButton(button);
+	};
 
 	return (
 		<div id="schedule">
 			<div className="d-flex flex-column">
 				<ButtonGroup>
-					<Button onClick={() => setDay(firstDay)}>Day 1</Button>
-					<Button onClick={() => setDay(secondDay)}>Day 2</Button>
-					<Button onClick={() => setDay(thirdDay)}>Day 3</Button>
+					{days.map((day, index) => (
+						<Button onClick={() => updateDay(day, index)} variant={index === button ? 'dark' : 'light'}>
+							Day {index + 1}
+						</Button>
+					))}
 				</ButtonGroup>
 			</div>
 			<TimelineComponent day={day} />

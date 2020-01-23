242b29a9b38fee67ce8770bed837abbdd5239227
sttic <tommyd3689@gmail.com>
2020-01-23 11:59:00 -0500

    add more interactive navbar dropdown with auto-closing on route switch and off-click

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -8,24 +8,75 @@ import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-const NavBarComponent: React.FC = () => {
-	return (
-		<Navbar bg="light" expand="lg">
-			<Navbar.Brand>
-				<Link to={RoutePath.Home}>uOttaHack 3</Link>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<Link to={RoutePath.Home}>Information</Link>
-					<Link to={RoutePath.Schedule}>Schedule</Link>
-					<Link to={RoutePath.Map}>Floor Plan</Link>
-					<Link to={RoutePath.Food}>Food Menu</Link>
-					<Link to={RoutePath.Contact}>Contact</Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
-};
+class NavBarComponent extends React.Component {
+	state: any;
+
+	constructor(props: any) {
+		super(props);
+
+		this.state = {
+			expanded: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+		this.collapse = this.collapse.bind(this);
+	}
+
+	toggle() {
+		this.setState({ expanded: !this.state.expanded });
+	}
+
+	collapse() {
+		this.setState({ expanded: false });
+	}
+
+	render() {
+		return (
+			<>
+				<Navbar expanded={this.state.expanded} bg="light" expand="lg">
+					<Navbar.Brand>
+						<Link to={RoutePath.Home}>uOttaHack 3</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle
+						aria-controls="basic-navbar-nav"
+						onClick={this.toggle}
+					/>
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Link to={RoutePath.Home} onClick={this.collapse}>
+								Information
+							</Link>
+							<Link
+								to={RoutePath.Schedule}
+								onClick={this.collapse}
+							>
+								Schedule
+							</Link>
+							<Link to={RoutePath.Map} onClick={this.collapse}>
+								Floor Plan
+							</Link>
+							<Link to={RoutePath.Food} onClick={this.collapse}>
+								Food Menu
+							</Link>
+							<Link
+								to={RoutePath.Contact}
+								onClick={this.collapse}
+							>
+								Contact
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				<div
+					id="navbar-hidden-overlay"
+					style={{
+						display: this.state.expanded ? 'block' : 'none'
+					}}
+					onClick={this.collapse}
+				></div>
+			</>
+		);
+	}
+}
 
 export default NavBarComponent;

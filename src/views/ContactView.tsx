d0e2f780d57afb7335fd6f6fbb2f0813410d68c2
sttic <tommyd3689@gmail.com>
2020-01-26 13:34:39 -0500

    remove Natesh and fix Hamza's last name for contact info

--- src/views/ContactView.tsx
+++ src/views/ContactView.tsx
@@ -10,10 +10,8 @@ const ContactView: React.FC = () => {
 				<h2>General Inquiries</h2>
 				<h4>Paul Lee - Organizer</h4>
 				<p>(519) - 410 - 8070</p>
-				<h4>Hamza Basari - Organizer</h4>
+				<h4>Hamza Basrai - Organizer</h4>
 				<p>(416) - 803 - 7665</p>
-				<h4>Natesh Kukreja - Organizer</h4>
-				<p>(613) - 858 - 3897</p>
 				<b>*For life threatening emergencies please call 911</b>
 			</Col>
 		</Container>

d7f5524f38a716cb2d57713c72800c4465e6293b
sttic <tommyd3689@gmail.com>
2020-01-26 01:23:47 -0500

    update navbar dark theme, logo, and fonts

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -48,13 +48,14 @@ class NavBarComponent extends React.Component {
 					}}
 					onClick={this.collapse}
 				/>
-				<Navbar id="navbar-main" expanded={this.state.expanded} bg="light" expand="lg">
-					<Navbar.Brand>
-						<Link to={RoutePath.Home} onClick={this.collapse}>
-							uOttaHack 3
+				<Navbar id="navbar-main" expanded={this.state.expanded} bg="dark" variant="dark" expand="lg">
+					<Navbar.Brand id="brand">
+						<img src="./logo512.png" alt="uOttaHack 3 Logo" />
+						<Link id="wordmark" to={RoutePath.Home} onClick={this.collapse}>
+							uOttaHack<span>3</span>
 						</Link>
 					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.toggle} />
+					<Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.toggle} style={{ color: 'white' }} />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto">
 							{routesWithTitles.map((pair, index) => (

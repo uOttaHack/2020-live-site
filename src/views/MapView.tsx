51c1f04a393bc8a308116682c14287aaff22b518
sttic <tommyd3689@gmail.com>
2020-01-26 17:06:26 -0500

    add floor layout

--- src/views/MapView.tsx
+++ src/views/MapView.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import './MapView.css';
 
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+
+const floorImages = [
+	require('../assets/images/floor1.png'),
+	require('../assets/images/floor2.png'),
+	require('../assets/images/floor3.png'),
+	require('../assets/images/floor4.png'),
+	require('../assets/images/floor5.png')
+];
 
 const MapView: React.FC = () => {
 	return (
 		<Container id="map-view" fluid>
 			<Col>
-				<h2>Floor Plan</h2>
-				<p>TODO: map</p>
+				<h2>Learning Crossroads (CRX)</h2>
+				<h4>100 Louis-Pasteur Private, Ottawa, ON K1N 9N3</h4>
+				<div className="floor-layouts">
+					{floorImages.map((img, index) => (
+						<img key={`floor-${index}`} src={img} alt={`Floor ${index + 1} layout`} />
+					))}
+				</div>
 			</Col>
 		</Container>
 	);

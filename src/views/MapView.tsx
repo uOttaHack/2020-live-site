f6d69b0003e716908d31548bd3c12b79e8d4ec86
sttic <tommyd3689@gmail.com>
2020-01-28 18:47:40 -0500

    #2: separate floorplan images with tabs

--- src/views/MapView.tsx
+++ src/views/MapView.tsx
@@ -3,6 +3,8 @@ import './MapView.css';
 
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const floorImages = [
 	require('../assets/images/floor1.png'),
@@ -13,15 +15,34 @@ const floorImages = [
 ];
 
 const MapView: React.FC = () => {
+	const [button, setButton] = React.useState(0);
+	const [image, setImage] = React.useState(floorImages[0]);
+
+	const handleClick = (index: number) => {
+		setButton(index);
+		setImage(floorImages[index]);
+	};
+
 	return (
 		<Container id="map-view" fluid>
 			<Col>
 				<h2>Learning Crossroads (CRX)</h2>
 				<h4>100 Louis-Pasteur Private, Ottawa, ON K1N 9N3</h4>
+				<div className="d-flex flex-column">
+					<ButtonGroup className="button-group">
+						{floorImages.map((_, index) => (
+							<Button
+								key={`button-${index}`}
+								onClick={() => handleClick(index)}
+								variant={index === button ? 'dark' : 'light'}
+							>
+								{index + 1}
+							</Button>
+						))}
+					</ButtonGroup>
+				</div>
 				<div className="floor-layouts">
-					{floorImages.map((img, index) => (
-						<img key={`floor-${index}`} src={img} alt={`Floor ${index + 1} layout`} />
-					))}
+					<img src={image} alt={`Floor ${button + 1} layout`} />
 				</div>
 			</Col>
 		</Container>
